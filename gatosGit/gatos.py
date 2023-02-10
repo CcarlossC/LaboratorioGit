615807323a9817c87448bab5f7f35f411b2e7af5
CcarlossC <124691128+CcarlossC@users.noreply.github.com>
2023-02-09 18:18:55 -0600

    Se crea el objeto gato3

--- gatosGit/gatos.py
+++ gatosGit/gatos.py
@@ -12,4 +12,12 @@ gato2.color = "cafe"
 gato2.edad = 3
 gato2.aniadirlista()
 
+gato3 = funcionesTarea.gato()
+gato3.nombre = "Estrella"
+gato3.color = "Blnaco"
+gato3.edad = 6
+gato3.aniadirlista()
+
+
+
 funcionesTarea.printlista()
